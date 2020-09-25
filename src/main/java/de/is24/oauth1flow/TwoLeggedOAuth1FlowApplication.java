3f32fae53915ab4bef2833aa9bd60e94aba5078d
Andre Weltsch <andre.weltsch@scout24.com>
2020-09-25 12:49:00 +0200

    DEVHUB-110: Remove unnecessary comments.

--- src/main/java/de/is24/oauth1flow/TwoLeggedOAuth1FlowApplication.java
+++ src/main/java/de/is24/oauth1flow/TwoLeggedOAuth1FlowApplication.java
@@ -18,20 +18,14 @@ public class TwoLeggedOAuth1FlowApplication {
     private static final String CONSUMER_SECRET = "yourConsumerSecret";
     private static final String PROTECTED_RESOURCE_ENDPOINT = "https://rest.sandbox-immobilienscout24.de/restapi/api/gis/v1.0/country/276/region";
 
-
     public static void main(String[] args) throws IOException, OAuthCommunicationException, OAuthExpectationFailedException, OAuthMessageSignerException {
-        // create a consumer object and configure it with the access
-        // token and token secret obtained from the service provider
         OAuthConsumer consumer = new DefaultOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
 
-        // create an HTTP request to a protected resource endpoint
         URL url = new URL(PROTECTED_RESOURCE_ENDPOINT);
         HttpURLConnection request = (HttpURLConnection) url.openConnection();
 
-        // sign the request
         consumer.sign(request);
 
-        // send the request
         request.connect();
         printResponse(request);
     }
